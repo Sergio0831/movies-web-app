58ec5095e24c2a172ed71623e0f68e08e936d5f8
Sergio0831 <sergejs.ivcenko@gmail.com>
2022-08-05 07:39:49 +0100

    Added react-toastify styles

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Frontend Mentor - Entertainment Web App solution
 
+This is a solution to the [Entertainment web app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/entertainment-web-app-J-UhgAW1X).
+
+Frontend Mentor challenges help you improve your coding skills by building realistic project.
+
+### Screenshot
+
+![Screenshot](./preview.png)
+
--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -1,6 +1,5 @@
 import classes from './Main.module.scss';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 type MainProps = {
   children?: React.ReactNode;
@@ -15,9 +14,6 @@ const Main = ({ children }: MainProps) => {
         autoClose={2000}
         newestOnTop={false}
         closeOnClick
-        toastClassName='toast'
-        bodyClassName='toast__body'
-        progressClassName='toast__progress'
         pauseOnFocusLoss
         draggable
         pauseOnHover
